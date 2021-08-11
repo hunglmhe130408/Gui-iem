e8ec7786d44127d3be52fac94e8c7ab9bad90c3d
hunglmhe130408 <88799436+hunglmhe130408@users.noreply.github.com>
2021-08-12 03:25:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,27 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Chúc mừng sinh nhật Vợ iu của anh nhoa ^^!',
+    introDesc: `Hôm nay anh chợt nhớ
+Ngày mẹ sinh ra em
+Em đẹp như que kem
+Và thật là xinh xắn
+Cuộc đời sao thật ngắn
+Chúc em sống thật vui
+Và luôn luôn yêu đời
+Cùng nhiều điều may mắn
+Em luôn luôn khỏe khoắn
+Học giỏi và thành công
+Mau mau sớm có chồng
+Để anh còn làm bố.`,
+    btnIntro: 'Thơm anh 1 cái 😘',
+    title: 'Em mong mún sinh nhật mình được nhận gì nèo 😙',
+    desc: 'Có phải những món mà dịch này em không được thưởng thức, những cái ôm, cái hôn của anh đi khắp cơ thể của em ^^ anh biết em mún nhìu thứ lắm lun nè :p ',
+    btnYes: 'Vâng, Sau dịch mình làm mọi thứ đó với nhau nha <33',
+    btnNo: 'Không, em chỉ muốn ăn anh hoy :3',
+    question:'Trời có lúc nắng lúc mưa, tuy vợ anh có lúc khùng lúc điên nhưng lúc nào anh cũng thương. Em đợi anh nhé ?, anh sẽ bù cho em!!!! <3 ',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hẹn em hệt dịch mình cùng đi chơi hen.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100009674654358' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
